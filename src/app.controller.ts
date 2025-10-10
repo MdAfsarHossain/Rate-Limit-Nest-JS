4592f46d28bb167038f33b81af199051c4188526
MdAfsarHossain <afsar291722@gmail.com>
2025-10-10 18:29:36 +0600

    Src folder all file added

---
+++ src/app.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+import { Throttle } from '@nestjs/throttler';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) { }
+
+  @Get()
+  @Throttle({ default: { limit: 3, ttl: 60000 } })
+  getHello(): string {
+    // return this.appService.getHello();
+    return 'This is a rete-limit route!'
+  }
+}
