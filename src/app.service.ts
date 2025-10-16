be798abdd6689c5fa46f8e34796babb9592ed01e
MdAfsarHossain <afsar291722@gmail.com>
2025-10-16 18:35:27 +0600

    Get hello function updated

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  // Get Hello
+  // Get Hello Function
   getHello(): string {
     return 'Welcome to the Nest JS World!';
   }
